86d0e0e37366a2cf630bd80b09d39a73bdb593b1
Vu Duy Bach <z5509166@ad.unsw.edu.au>
2024-05-05 16:49:59 +1000

    Adjust config parameters related to colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,8 @@
-local colorscheme = "default"
+local colorscheme = "kanagawa-dragon"
 
+-- Settting local variable status_ok; do protected call with vim.cmd
+-- .. for concatenate
+-- If you don't need the return value; use underscore
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
