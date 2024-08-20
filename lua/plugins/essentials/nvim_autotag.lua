0b780058d35ff28255d58eaa4888ab89aa04b7fb
Vu Duy Bach <z5509166@ad.unsw.edu.au>
2024-08-20 13:25:55 +1000

    Added emmet_vim and nvim_autotag for working with HTML

---
+++ lua/plugins/essentials/nvim_autotag.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "windwp/nvim-ts-autotag",
+    event = "VeryLazy",
+    config = function ()
+      local status_ok, ntags = pcall(require, "nvim-ts-autotag")
+      if not status_ok then
+        return
+      end
+
+      ntags.setup({
+        opts = {
+          -- Defaults
+          enable_close = true, -- Auto close tags
+          enable_rename = true, -- Auto rename pairs of tags
+          enable_close_on_slash = false -- Auto close on trailing </
+        },
+        -- Also override individual filetype configs, these take priority.
+        -- Empty by default, useful if one of the "opts" global settings
+        -- doesn't work well in a specific filetype
+        per_filetype = {
+          ["html"] = {
+            enable_close = false
+          }
+        }
+      })
+    end
+  }
+}
